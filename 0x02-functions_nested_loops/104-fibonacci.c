3ef8fceb02ab8cfb239bc497e32045c0bf137596
root <cse21-091>
2023-08-15 17:30:28 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,72 @@
-#include <stdio.h>
+#include"main.h"
 
 /**
- * main - Prints the first 98 Fibonacci numbers.
+ * numLength - returns the length of string
  *
- * Return: Always 0.
- */
-int main(void)
+ * @num: operand number
+ *
+ * Return: number of digits
+*/
+
+int numLength(int num)
 {
-	unsigned long fib1 = 1, fib2 = 2, nextFib;
-	int count;
+	int length = 0;
 
-	printf("%lu, %lu", fib1, fib2);
+	if (!num)
+		return (1);
 
-	for (count = 2; count < 98; count++)
+	while (num)
 	{
-		nextFib = fib1 + fib2;
-		printf(", %lu", nextFib);
-		fib1 = fib2;
-		fib2 = nextFib;
+		num = num / 10;
+		length += 1;
 	}
 
-	printf("\n");
+	return (length);
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the first 98 Fibonacci numbers
+ *	starting with 1 and 2 followed by a new line
+ *
+ *	Solution was copied from Nobert Patrick
+ *	Wise, github handle: Trikcode
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int count, initial0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+
+	for (count = 1; count <= 98; ++count)
+	{
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLength(f1);
+
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			--initial0s;
+		}
+
+		printf("%lu", f1);
+
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
+
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
+	}
 
 	return (0);
 }
