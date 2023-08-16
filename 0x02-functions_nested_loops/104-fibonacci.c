4590f7d0ea95aa73ab804fba9c76db1eaf24896c
root <cse21-091>
2023-08-15 17:45:18 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,58 +1,28 @@
 #include "main.h"
-/**
- * * numbFunc - length of string
- * *
- * * @nbr: number
- * * Return: number
- * *
- */
-int numbFunc(int nbr)
-{
-	int l = 0;
+#include <stdio.h>
 
-	if (!nbr)
-		return (1);
-	while (nbr)
-	{
-		nbr = nbr / 10;
-		l += 1;
-	}
-	return (l);
+int main() {
+    int a = 1, b = 2;
+    int count = 2; // Count starts from 2 because we already have the first two Fibonacci numbers (1 and 2)
+
+    printf("%d, %d, ", a, b);
+
+    while (count < 98) {
+        int next = a + b;
+        printf("%d", next);
+
+        // Add the comma and space unless it's the last number
+        if (count < 97) {
+            printf(", ");
+        } else {
+            printf("\n");
+        }
+
+        a = b;
+        b = next;
+        count++;
+    }
+
+    return 0;
 }
-/**
- * * main - Entry point
- * *
- * * Description: Fibonacci numbers
- * * Return: always 0
- *
- */
-int main(void)
-{
-	int count, i;
-	unsigned long f = 1, f1 = 2, sum, mx = 100000000, f2 = 0, f3 = 0, sum1 = 0;
 
-	for (count = 1; count <= 98; count++)
-	{
-		if (f2 > 0)
-			printf("%lu", f2);
-		i = numbFunc(mx) - 1 - numbFunc(f);
-		while (f2 > 0 && i > 0)
-		{
-			printf("%d", 0);
-			i--;
-		}
-		printf("%lu", f);
-		sum = (f + f1) % mx;
-		sum1 = f2 + f3 + (f + f1) / mx;
-		f = f1;
-		f2 = f3;
-		f1 = sum;
-		f3 = sum1;
-		if (count != 98)
-			printf(", ");
-		else
-			printf("\n");
-	}
-
-	return (0);
-}
