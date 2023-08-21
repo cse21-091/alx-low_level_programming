a3afcaa440f3263eb209b6090a91b92a2eedce17
root <cse21-091>
2023-08-21 14:19:37 -0700

    correction of 2 and  I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
- * _strlen - determine the length of a string 
- * @s:The pointer to the string 
- * Return : the string length of s 
- * */
-int _strlen(Char *s)
+ * _strlen - determine the length of a string
+ * @s:The pointer to the string
+ * Return: the string length of s
+ */
+int _strlen(char *s)
 {
-	int y;
-	int x;
+int y;
+int x;
 
-	x = 0;
-	y = 0;
-	while (s[x] != 0)
-	{
-		y++;
-		x++;
-	}
-	return (y);
+x = 0;
+y = 0;
+while (s[x] != 0)
+{
+y++;
+x++;
+}
+return (y);
 }
