fded0767eeec3690d9f1016b3db7eaa4a25060bf
root <cse21-091>
2023-09-12 13:03:50 -0700

    A door is what a dog is perpetually on the wrong side of mandatory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,54 +1,72 @@
-#ifndef NEW_DOG_H
-#define NEW_DOG_H
-
+#include <stdio.h>
 #include <stdlib.h>
+#include "dog.h"
 
 /**
- * struct dog - Represents a dog
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
+ * new_dog - Creates a new dog structure.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: A pointer to the new dog structure (Success), NULL otherwise.
  */
-struct dog
+dog_t *new_dog(char *name, float age, char *owner)
 {
-    char *name;
-    float age;
-    char *owner;
-};
+    dog_t *dog;
+    int name_len, owner_len;
 
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
+    dog = malloc(sizeof(dog_t));
+    if (dog == NULL)
+        return (NULL);
 
-/**
- * _strlen - Returns the length of a string
- * @s: String to evaluate
- *
- * Return: The length of the string
- */
-int _strlen(char *s);
+    name_len = 0;
+    owner_len = 0;
 
-/**
- * _strcpy - Copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
- * @dest: Pointer to the buffer in which we copy the string
- * @src: String to be copied
- *
- * Return: The pointer to dest
- */
-char *_strcpy(char *dest, char *src);
+    while (name[name_len] != '\0')
+        name_len++;
 
-/**
- * new_dog - Creates a new dog
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
- *
- * Return: Pointer to the new dog (Success), NULL otherwise
- */
-dog_t *new_dog(char *name, float age, char *owner);
+    while (owner[owner_len] != '\0')
+        owner_len++;
 
-#endif /* NEW_DOG_H */
+    dog->name = malloc(sizeof(char) * (name_len + 1));
+    dog->owner = malloc(sizeof(char) * (owner_len + 1));
+
+    if (dog->name == NULL || dog->owner == NULL)
+    {
+        free(dog->name);
+        free(dog->owner);
+        free(dog);
+        return (NULL);
+    }
+
+    for (int i = 0; i <= name_len; i++)
+        dog->name[i] = name[i];
+
+    for (int i = 0; i <= owner_len; i++)
+        dog->owner[i] = owner[i];
+
+    dog->age = age;
+
+    return (dog);
+}
+
+int main(void)
+{
+    dog_t *my_dog;
+
+    my_dog = new_dog("Poppy", 3.5, "Bob");
+    if (my_dog == NULL)
+    {
+        printf("Failed to create a new dog.\n");
+        return (1);
+    }
+
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+
+    free(my_dog->name);
+    free(my_dog->owner);
+    free(my_dog);
+
+    return (0);
+}
 
