a855a70eca804bfaf5ce228ad3d080d11d73a22c
root <cse21-091>
2023-09-12 13:07:44 -0700

    A door is what a dog is perpetually on the wrong side of..

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,72 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
-
+#include <stdlib.h>
 /**
- * new_dog - Creates a new dog structure.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- *
- * Return: A pointer to the new dog structure (Success), NULL otherwise.
+ **new_dog - Creates a new dog
+ *@name: Input dog name
+ *@age: Input dog age
+ *@owner: Input owner
+ *Return: pointer or NULL
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
-    int name_len, owner_len;
+	unsigned int nl, ol, i;
+	dog_t *dog;
 
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-        return (NULL);
-
-    name_len = 0;
-    owner_len = 0;
-
-    while (name[name_len] != '\0')
-        name_len++;
-
-    while (owner[owner_len] != '\0')
-        owner_len++;
-
-    dog->name = malloc(sizeof(char) * (name_len + 1));
-    dog->owner = malloc(sizeof(char) * (owner_len + 1));
-
-    if (dog->name == NULL || dog->owner == NULL)
-    {
-        free(dog->name);
-        free(dog->owner);
-        free(dog);
-        return (NULL);
-    }
-
-    for (int i = 0; i <= name_len; i++)
-        dog->name[i] = name[i];
-
-    for (int i = 0; i <= owner_len; i++)
-        dog->owner[i] = owner[i];
-
-    dog->age = age;
-
-    return (dog);
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	for (nl = 0; name[nl]; nl++)
+		;
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < nl; i++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
 }
-
-int main(void)
-{
-    dog_t *my_dog;
-
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    if (my_dog == NULL)
-    {
-        printf("Failed to create a new dog.\n");
-        return (1);
-    }
-
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-
-    free(my_dog->name);
-    free(my_dog->owner);
-    free(my_dog);
-
-    return (0);
-}
-
