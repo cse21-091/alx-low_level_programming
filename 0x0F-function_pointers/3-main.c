a43fc1829b216305b0202beccc1be79477bd24dc
root <cse21-091>
2023-09-13 05:21:02 -0700

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -37,7 +37,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(op)(num1, num2));
+	printf("%d\n", get_op_func(op)(N1, N2));
 
 	return (0);
 }
