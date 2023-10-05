ca65581abd0e40cfa97d25cbd18d7aef605586cb
root <cse21-091>
2023-10-05 14:11:23 -0700

    0-read_textfile.c corrections

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,19 +11,19 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buff;
-	ssize_t fdta;
+	ssize_t fd;
 	ssize_t w;
 	ssize_t t;
 
-	fdta = open(filename, O_RDONLY);
-	if (fdta == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 	buff= malloc(sizeof(char) * letters);
-	t = read(fdta, buff, letters);
+	t = read(fd, buff, letters);
 	w = write(STDOUT_FILENO, buff, t);
 
 	free(buff);
-	close(fdta);
+	close(fd);
 	return (w);
 }
 
